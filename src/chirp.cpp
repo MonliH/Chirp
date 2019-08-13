23051142eeabe6b7c6c7dcbeacdcd9b8514584b1
Maxence Cossette <maxencek7@gmail.com>
2019-08-12 21:40:21 -0400

    doing pretty much next to nothing

--- src/chirp.cpp
+++ src/chirp.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[])
     std::vector<std::string> args;
 
    for(int i = 1; i < argc; i++)
-    {
+   {
         std::string arg = argv[i];
         args.push_back(arg);
-    }
+   }
 
     cli::parse_Command(args);
     return 0;
